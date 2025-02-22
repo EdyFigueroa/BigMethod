f0dfd5cf6ecbb3a550d92c2bb3d876e70ded333f
Edy Jared Figueroa Lizárraga <159922867+EdyFigueroa@users.noreply.github.com>
2025-02-22 02:27:15 +0000

    Datos pedidos

--- BigMethod.java
+++ BigMethod.java
@@ -10,11 +10,28 @@ public class BigMethod {
     public static void main(String [] args) {
         printHojaPresentacion();
 
-        System.out.println("\nPregunta del problema: ___");
+        // Preguntar por los parámetros del problema
+        System.out.print("- Pregunta del problema: > ");
+        String pregunta = sc.nextLine();
+        System.out.print("- Concepto: > ");
+        String concepto = sc.nextLine();
+        System.out.print("- Unidad de medida: > ");
+        String unidad = sc.nextLine();
+
+        // Mostrar el menú
+        System.out.println("=================================================================");
+        int opcion = 0;
+        while (opcion != 10) {
+            imprimirMenu();
+        }
     }
 
+    /**
+     * Este método imprime la hoja de presentación
+     */
     public static void printHojaPresentacion() {
         // Datos de la escuela
+        System.out.println("=================================================================");
         System.out.println("              Instituto Tecnológico de Culiacán");
         System.out.println("               Ing en Sistemas Computacionales");
 
@@ -27,5 +44,6 @@ public class BigMethod {
         System.out.println("\n         Este programa ejecuta la solución de problemas\r\n" + //
                         "     utilizando los procesos lógicos de cálculo de Raíces de\r\n" + //
                         "          una ecuación con diversos métodos numéricos.");
+        System.out.println("=================================================================");
     }
 }
