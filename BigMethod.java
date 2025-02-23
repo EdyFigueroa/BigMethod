4f1ee353038a8fffce863d60cd5a794fedb5f5d7
Edy Jared Figueroa Lizárraga <159922867+EdyFigueroa@users.noreply.github.com>
2025-02-23 00:52:58 +0000

    Calcular siguiennte (secante)

--- BigMethod.java
+++ BigMethod.java
@@ -20,9 +20,35 @@ public class BigMethod {
 
         // Mostrar el menú
         System.out.println("=================================================================");
-        int opcion = 0;
+        imprimirMenu();
+        System.out.print("- Teclee su opción: > ");
+
+        int opcion = 0;        
         while (opcion != 10) {
-            imprimirMenu();
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    // Método de la secante
+                    System.out.println("=================================================================");
+                    System.out.println("                      MÉTODO DE LA SECANTE\n");
+                    metodoSecante();
+                    break;
+
+                case 2:
+                    System.out.println("Metodo de Newton rapsody seleccionado. Turbo nice!");
+                    break;
+
+                case 10:
+                    // Caso de salir
+                    System.out.println("matate pues cffff");
+                    break;
+            
+                default:
+                    // Caso en el que la opción no esté en el menú
+                    System.out.print("- /!\\ Teclee una opción valida: > ");
+                    break;
+            }
         }
     }
 
@@ -46,4 +72,25 @@ public class BigMethod {
                         "          una ecuación con diversos métodos numéricos.");
         System.out.println("=================================================================");
     }
+
+    public static void imprimirMenu() {
+        System.out.println("[1] Método de la Secante");
+        System.out.println("[2] Método de Newton Raphson");
+        System.out.println("[10] F I N\n");
+    }
+
+    public static void metodoSecante() {
+        // Imprimir cabecera de la tabla
+        System.out.println("┌──┬──────────┬──────────┬──────────┬──────────┬──────────┬──────────┐");
+        System.out.println("│N°│    x1    │   f(x1)  │    x2    │   f(x2)  │    x3    │   f(x3)  │");
+        System.out.println("├──┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┤");
+
+        Secante secante = new Secante(0.1, 0.6, 0.00001, 40);
+        double[] resultados = secante.nextCalc();
+
+        // Imprimir nueva línea
+        secante.printLine(resultados);
+    }
+
+
 }
---
+++ Secante.java
@@ -0,0 +1,78 @@
+public class Secante {
+    // Atributos
+    double x1; // Límite inferior de la busqueda
+    double x2; // Límite superior de la busqueda
+    double error; // Error tolerado para el cálculo
+    int calculos; // Número de cálculos límite
+    static int calculoActual = 0; // Cálculo en el que se encuentra actualmente
+    
+        // Constructores
+        public Secante(double x1, double x2, double error, int calculos) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.error = error;
+            this.calculos = calculos;
+        }
+    
+        // Métodos de la clase
+    
+        /**
+         * Esta función calcula una nueva iteración para el método de la secante.
+         * @return Regresa un arreglo con toda la información necesaria para imprimir una línea de la tabla.
+         */
+        public double[] nextCalc() {
+            // Creamos el arreglo que regresará este método
+            // Deberá regresar: [N, x1, f(x1), x2, f(x2), x3, f(x3)]
+            double[] arreglo = new double[7];
+    
+            // 1. N: Número del cálculo
+            calculoActual++; // Sumar 1 al cálculo
+            arreglo[0] = calculoActual;
+    
+            // 2. x1: Límite inferior
+            arreglo[1] = x1;
+    
+            // 3. f(x1): Función evaluada en el límite inferior
+            double u = ((10*x1)/9) - 1;
+            double fx1 = Math.asin(u) + u*(Math.sqrt(1 - Math.pow(u, 2))) - 1.100144;
+            arreglo[2] = fx1;
+    
+            // 4. x2: Límite superior
+            arreglo[3] = x2;
+    
+            // 5. f(x2): Función evaluada en el límite superior
+            u = ((10*x2)/9) - 1;
+            double fx2 = Math.asin(u) + u*(Math.sqrt(1 - Math.pow(u, 2))) - 1.100144;
+            arreglo[4] = fx2;
+    
+            // 6. x3: Nuevo límite
+            double x3 = x1 - ((x1 - x2)*fx1)/(fx1-fx2);
+            arreglo[5] = x3;
+    
+            // 7. f(x3): Función evaluada en el nuevo límite
+            u = ((10*x3)/9) - 1;
+            double fx3 = Math.asin(u) + u*(Math.sqrt(1 - Math.pow(u, 2))) - 1.100144;
+            arreglo[6] = fx3;
+    
+            // Regresar arreglo
+            return arreglo;
+        }
+    
+        public static void printLine(double[] r) {
+            String x1 = truncar(r[1], 6);
+            String fx1 = truncar(r[2], 9);
+            String x2 = truncar(r[3], 9);
+            String fx2 = truncar(r[4], 9);
+            String x3 = truncar(r[5], 9);
+            String fx3 = truncar(r[6], 9);
+
+            String linea = String.format("│%2d│%10s│%10s│%10s│%10s│%10s│%10s│",
+                calculoActual, x1, fx1, x2, fx2, x3, fx3);
+            System.out.println(linea);
+    }
+
+    public static String truncar(double valor, int limite) {
+        String res = String.valueOf(valor);
+        return res.substring(0, Math.min(limite, res.length()));
+    }
+}
